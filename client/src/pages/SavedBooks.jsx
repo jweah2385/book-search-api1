00abe4e76f4bb6e1bdb9adc0c0bffcfc6d965d47
James Weah <jweah@aggies.ncat.edu>
2023-11-15 21:56:44 -0500

    rebuilt pushing to new repo

---
+++ client/src/pages/SavedBooks.jsx
@@ -0,0 +1,105 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unknown-property */
+
+// import { useState, useEffect } from "react";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+
+// import { getMe, deleteBook } from "../utils/API";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
+
+import { useMutation, useQuery } from "@apollo/client";
+import { GET_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+
+const SavedBooks = () => {
+  const { loading, data } = useQuery(GET_ME);
+
+  const [deleteBook, _] = useMutation(REMOVE_BOOK, {
+    refetchQueries: [
+      { query: GET_ME }
+    ],
+    awaitRefetchQueries: true,
+  });
+
+  const userData = data?.me || {};
+  console.log(userData);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      await deleteBook({
+        variables: { bookId },
+      });
+
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn't here yet, say so
+  if (loading) {
+    return <h2>LOADING...</h2>;
+  }
+
+  return (
+    <>
+      <div fluid className="text-light bg-dark p-5">
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </div>
+      <Container>
+        <h2 className="pt-5">
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? "book" : "books"
+              }:`
+            : "You have no saved books!"}
+        </h2>
+        <Row>
+          {userData.savedBooks.map((book) => {
+            return (
+              <Col key={book.bookId} md="4">
+                <Card border="dark">
+                  {book.image ? (
+                    <Card.Img
+                      src={book.image}
+                      alt={`The cover for ${book.title}`}
+                      variant="top"
+                    />
+                  ) : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className="small">Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    <Button
+                      className="btn-block btn-danger"
+                      onClick={() => handleDeleteBook(book.bookId)}
+                    >
+                      Delete this Book!
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default SavedBooks;
